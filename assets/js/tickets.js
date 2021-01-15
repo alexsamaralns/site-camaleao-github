65ce4609f494d0569557d549c7991317cc544135
Alex Amaral <alexsamaralns@gmail.com>
2021-01-15 13:26:15 -0300

    Functions update 18

--- assets/js/tickets.js
+++ assets/js/tickets.js
@@ -2,40 +2,62 @@ let token = "f0ae371e-5848-45eb-a109-b3d66a105d98";
 let url = "https://api.movidesk.com/public/v1/tickets?token=" + token + "&returnAllProperties=false";
 
 var data = new Date();
-var day = data.getDate();,
+var day = data.getDate();
 
 var mouth = data.getMonth() + 1;
 var year = data.getFullYear();
 var hour = data.getHours();
 var min = data.getMinutes();
 var msec = data.getMilliseconds();
-var dateTime = year + "-" + mouth + "-" + day + "T" + hour + ":" +
-  min + ":" + msec;
+var dateTime = year + "-" + mouth + "-" + day + "T" + hour + ":" + min + ":" + msec;
+
+var form = document.getElementById('form');
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let subject = document.getElementById('subject');
+var message = document.getElementById('message');
+
+var myJSon = {
+ "type": 2,
+ "subject": "Assunto",
+ "category": "Dúvida",
+ "urgency": "Baixa",
+ "status": "Aguardando",
+ "justification": "Retorno do cliente",
+ "createdDate": "2021-01-15T14:36:01+00:00",
+ "createdBy":{
+     "id":"7d1d8aba-cbdd-44cf-"
+  },
+  "clients": [
+        {
+            "id":"7d1d8aba-cbdd-44cf-"
+        }
+    ],"Actions" :[
+        {
+            "type":2,
+            "Description" :"COLOCAR AQUI A MENSAGEM DO CLIENTE"
+        }
+    ]
+};
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  console.log(JSON.stringify(myJSon));
+  $.ajax({
+  type: "POST",
+  contentType: 'application/json',
+  url: "https://api.movidesk.com/public/v1/tickets?token=f0ae371e-5848-45eb-a109-b3d66a105d98&returnAllProperties=false",
+  dataType: "json",
+  data: JSON.stringify(myJSon),
+  sucess: function (data){
+      alert('Sucess');
+  },
+  error: function (err) {
+      console.log(err);
+  }
+});
+});
 
 function postTicket() {
-  fetch(url, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  //body: JSON.stringify(data),
-  RequestBody:
-    {
-     "type": 2,
-     "subject": "Assunto Teste",
-     "category": "Dúvida",
-     "urgency": "Média",
-     "status": "Status",
-     "justification": "Justificativa",
-     "createdDate": dateTime,
-      ... Demais colunas no formato do layout acima
-    }
-})
-  .then((response) => response.json())
-  .then((data) => {
-    console.log("Success:", data);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+  //console.log("message = " + message);
+  /**/
 }
