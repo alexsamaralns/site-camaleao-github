8a70316c7ccddae446598fda75d7392a2ce3bc84
alexsamaralns <alexsamaralns@gmail.com>
2021-03-23 15:07:55 -0300

    Implementação do chat

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,7 +82,6 @@ function scrollToSection(idSection) {
     behavior: "smooth"
   });
 }
-<<<<<<< HEAD
 
 var btnTestFree = document.getElementById('test-free-btn');
 
@@ -96,5 +95,27 @@ function shake() {
 }
 
 shake();
-=======
->>>>>>> 352e58c6d5f97c6b84eec65dd47f2f073f9cbadf
+
+function mask(o, f) {
+  setTimeout(function () {
+      var v = mphone(o.value);
+      if (v != o.value) {
+          o.value = v;
+      }
+  }, 1);
+}
+
+function mphone(v) {
+  var r = v.replace(/\D/g, "");
+  r = r.replace(/^0/, "");
+  if (r.length > 10) {
+      r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+  } else if (r.length > 5) {
+      r = r.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+  } else if (r.length > 2) {
+      r = r.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+  } else {
+      r = r.replace(/^(\d*)/, "($1");
+  }
+  return r;
+}
